437c54f67b8cd128db61491a88feeb0575415072
Lucas Thullier <lucas.thullier@outlook.fr>
2020-06-03 16:33:35 +0200

    -need to be rework- A function who sort number inside an array and who find index to insert a number in orderly manner

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -1,7 +1,27 @@
+  
 function getIndexToIns(arr, num) {
   arr.sort(function(a,b){return a - b;});
-  console.log(arr);
-  return num;
+  let index = 0;
+  
+  if (arr[0] == null){
+    index = 0;
+    return index;   
+  }
+  if (num > arr[arr.length-1]){
+    index = arr.length;
+    return index;
+  }else{
+    for(let i = 0 ; i <= arr.length ; i++){
+      if (num == arr[i]){
+        index = arr.indexOf(arr[i]);
+        return index;
+      }
+    
+      else if (num > arr[i] && num < arr[i+1]){
+        index = arr.indexOf(arr[i+1]);
+        return index;
+      } 
+    }
+  }
 }
-
-getIndexToIns([1, 10, 5, 3, 20, 3], 5);
+getIndexToIns([], 1);
