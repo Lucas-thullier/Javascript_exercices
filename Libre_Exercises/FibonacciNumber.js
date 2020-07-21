08a75b83a0cdacc95214c95d71c73ea42665a9f3
Lucas Thullier <lucas.thullier@outlook.fr>
2020-07-21 10:40:17 +0200

    add file Libre_Exercices containing FibonacciNumber exercise

---
+++ Libre_Exercises/FibonacciNumber.js
@@ -0,0 +1,18 @@
+let array = [];
+
+let nbA = 0;
+let nbB = 1;
+let nbC = nbA + nbB;
+
+array.push(nbA);
+array.push(nbB);
+array.push(nbC);
+
+let i = 0;
+while (array[array.length-1] <= 100) {
+  array.push(array[array.length-2] + array[array.length-1]);
+  
+}
+
+
+console.log(array);
